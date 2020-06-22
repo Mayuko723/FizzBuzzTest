e2f44b01f70e00dd2dbca43e869e523fe0751be8
Mayuko Usami <mayuko.usami@konicaminolta.com>
2020-06-22 14:54:27 +0900

    改行を実装

--- FizzBuzzTest.ino
+++ FizzBuzzTest.ino
@@ -7,20 +7,25 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  t=1+millis()/200;
   
+
+  for(int x=0; x<=100; x++){
+    t=1+millis()/200;
   if(t%3==0 && t%5==0) {
     Serial.print("FizzBuzz");
   } else {
     if (t%3==0) {
-      Serial.print("Fizz    \n");
+      Serial.print("Fizz");
     } else {
       if (t%5==0) {
-       Serial.print("Buzz    \n");
+       Serial.print("Buzz");
       } else {
-       Serial.print("        \n");
+       Serial.print("");
       }
     }
   } 
+  Serial.println("");
   Serial.print(t);
+  delay(200);
+  }
 }
