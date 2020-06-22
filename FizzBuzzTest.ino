c36bb692a8a989e8256f49b9017aa37d5e67035d
Mayuko Usami <mayuko.usami@konicaminolta.com>
2020-06-22 14:33:46 +0900

    FizzBuzzTestqを実行するプログラム

--- FizzBuzzTest.ino
+++ FizzBuzzTest.ino
@@ -1,9 +1,26 @@
+int t=0;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  t=1+millis()/200;
+  
+  if(t%3==0 && t%5==0) {
+    Serial.print("FizzBuzz");
+  } else {
+    if (t%3==0) {
+      Serial.print("Fizz    \n");
+    } else {
+      if (t%5==0) {
+       Serial.print("Buzz    \n");
+      } else {
+       Serial.print("        \n");
+      }
+    }
+  } 
+  Serial.print(t);
 }
